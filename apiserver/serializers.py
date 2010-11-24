8b4114227b719004ebbbba2e0c424b9c1f34ec86
Stijn Debrouwere <stijn@stdout.be>
2010-11-24 15:58:19 -0800

    Big refactor of the serialization process. JSON, XML and YAML now spit out data structures that mimick each other, and none of these formats chokes on complex datatypes anymore.

--- apiserver/serializers.py
+++ apiserver/serializers.py
@@ -5,73 +5,124 @@ import yaml
 from flask import render_template
 
 """
-The resource serializer depends on a 'properties' attribute when serializing objects, 
-both for XML and JSON. This attribute should be a dictionary. The simplest implementation
-is as follows:
+Objects may specify a 'facade' property, which will be used instead of its public properties
+when serializing the object. This attribute may be a list or a string, but is most often a 
+dictionary, e.g. a subset of self.__dict__. The simplest implementation is as follows:
 
 class MyClass(object):
     @property
-    def properties(self):
+    def facade(self):
         return self.__dict__
-        
-If I ever get around to refactoring this, a better approach to the serialization would to
-objects --> dumbed-down representation (strings, dicts, lists) --> serialize
 """
+
+"""
+Prior to serialization, we recursively simplify the object structure to the lowest common denominator,
+that is, everything that is easily representable in JSON. That way, we can make sure that the output
+of each serialization format will stay close to the other formats, making it easy for users to switch
+between formats, and easy for us to document our API without having to explain differences in how each
+format renders the same resource.
+"""
+
+class Simplifier(object):
+    def simplify_list(self, obj):
+        simplify = self.__class__()
+        return [simplify(item) for item in obj]
+    
+    def simplify_dict(self, obj):
+        simplify = self.__class__()
+        simple = {}
+        for key, value in obj.items():
+            simple[key] = simplify(value)
+        return simple
+    
+    def simplify_tuple(self, obj):
+        return list(obj)
+    
+    def simplify_string(self, obj):
+        return unicode(obj)
+        
+    def simplify_object(self, obj):
+        if hasattr(obj, 'facade'):
+            facade = obj.facade
+        else:
+            attrs =  obj.__dict__
+            for attr in attrs.keys():
+                if attr.startswith('_'):
+                    del attrs[attr]
+            facade = attrs
+            
+        facade = self.simplify_dict(facade)
+        
+        return {obj.__class__.__name__.lower(): facade}
+    
+    def __call__(self, obj):
+        if isinstance(obj, list):
+            return self.simplify_list(obj)
+        elif isinstance(obj, dict):
+            return self.simplify_dict(obj)
+        elif isinstance(obj, tuple):
+            return self.simplify_tuple(obj)
+        elif isinstance(obj, str):
+            return self.simplify_string(obj)
+        elif hasattr(obj, '__dict__'):
+            return self.simplify_object(obj)
+        else:
+            return obj
+
+simplify = Simplifier()
 
 class Resource(object):
     def __init__(self, obj):
         self.obj = obj
 
-    # dead-simple serialization, because pyxser is a bit over the top
-    # and because we want XML that completely mirrors the JSON output
-    # (so no attributes or other fancy stuff)
-    def to_xml(self, parent='item'):
+    # dead-simple serialization, because we want XML  
+    # that completely mirrors the JSON output
+    # (no attributes or other fancy stuff)        
+    def to_xml(self, parent='item', depth=0):   
         out = ''
-        
-        if isinstance(self.obj, list) or isinstance(self.obj, tuple):
+        simple = True
+    
+        if isinstance(self.obj, list):
+            simple = False
+            out += '\n'
+            if parent == 'item':
+                parent = 'list'
             for item in self.obj:
-                out += Resource(item).to_xml()
-            return '<{0}>{1}</{0}>'.format(parent, out)
+                out += Resource(item).to_xml('item', depth+1)
         elif isinstance(self.obj, dict):
-            for attr in self.obj:
-                prop = self.obj[attr]
-                out += Resource(prop).to_xml(attr)
-            return '<{0}>{1}</{0}>'.format('item', out)            
-        elif hasattr(self.obj, 'properties'):
-            name = self.obj.__class__.__name__.lower()
-            for attr in self.obj.properties:
-                prop = self.obj.properties[attr]
-                out += Resource(prop).to_xml(attr)
-            return '<{0}>{1}</{0}>'.format(name, out)
+            simple = False
+            out += '\n'
+            for key, value in self.obj.items():
+                out += Resource(value).to_xml(key, depth+1)
         else:
-            return '<{0}>{1}</{0}>'.format(parent, self.obj)
+            out += unicode(self.obj)
+        
+        start_tabs = end_tabs = ''
+        for i in range(depth):
+            start_tabs += '    '
+        if not simple:
+            end_tabs = start_tabs
+        return '{0}<{1}>{2}{3}</{1}>\n'.format(start_tabs, parent, out, end_tabs)
         
     def to_json(self):
-        def object_encoder(obj):
-            if hasattr(obj, 'properties'):
-                return obj.properties
-            raise TypeError(repr(obj) + " is not JSON serializable")  
-        return simplejson.dumps(self.obj, default=object_encoder, indent=4)
+        return simplejson.dumps(self.obj, indent=4)
     
     def to_html(self, template):
         return render_template(template, **self.obj.__dict__)
     
     def to_yaml(self):
-        def obj_representer(dumper, data):
-            return dumper.represent_mapping('tag:yaml.org,2002:map', data.properties)
         def unicode_representer(dumper, data):
             return yaml.ScalarNode('tag:yaml.org,2002:str', data)
     
         yaml.add_representer(unicode, unicode_representer)
-        if hasattr(self.obj, 'properties'):
-            yaml.add_representer(self.obj.__class__, obj_representer)
         return yaml.dump(self.obj, default_flow_style=False)
     
     def to_txt(self):
         return unicode(self.obj)
 
 def formatted_response(obj, html_template=None, formats=['xml', 'json', 'yaml']):
-    resource = Resource(obj)
+    simple_obj = simplify(obj)
+    resource = Resource(simple_obj)
     formatters = {
         'xml': resource.to_xml, 
         'json': resource.to_json, 
@@ -85,3 +136,33 @@ def formatted_response(obj, html_template=None, formats=['xml', 'json', 'yaml'])
             del formatters[formatter]
 
     return formatters
+
+"""
+Tests (todo: convert to proper unit tests)
+
+class Fancy(object):
+    def __init__(self):
+        self.funicode = u'some name'
+        self.fstring = 'mr'
+        self.flist = ['one', 'two', 'three']
+        self.ftuple = ('one', 'three', 'five')
+        self.fdict = {'one': 1, 'two': 2}
+        self._private = 'private'
+        
+        @property
+        def facade(self):
+            return self.__dict__
+
+from pprint import pprint
+
+f = Fancy()
+f.deep = [Fancy(), Fancy(), 'unfancy']
+        
+simplify = Simplifier()
+simple_f = simplify(f)
+
+resource = Resource([simple_f])
+pprint(simple_f)
+print simplejson.dumps(simple_f, indent=4)
+print(resource.to_yaml())
+"""
